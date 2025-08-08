ee4ee382ee9bfd61710e76d4e63d708ac77c86de
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-08-08 12:04:23 -0300

    api

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const app = express()
-const port = 3000
-
+const port = 3001
+app.use(express.json())
+const usuarios = []
+app.post('/usuarios', (req, res)=>{
+    const usuario = req.body //"pega" os dados da requesição(/pergunta/dados tipo, de uma compra)
+    usuarios.push(usuario) // adiciona o usuário na lista de usuários
+    res.send("Usuário cadastrado com sucesso!")
+})
+app.get('/usuarios', (req, res)=>{
+    res.send(usuarios)
+})
 app.get('/ola', (req, res)=>{
     res.send("Olá 3ºDS!")
 })
-
 app.listen(port, ()=>{
     console.log("API rodando...")
 })
